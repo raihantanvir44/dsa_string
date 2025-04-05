5ba51db9a94220af9ff678070f622680b3098751
Raihan Tanvir <raihantanvir2005@gmail.com>
2025-04-05 22:42:42 +0600

    Add files via upload

---
+++ reverse_word.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+string reversword(string s){
+    int n=s.length();
+    string ans;
+
+    reverse(s.begin(),s.end());
+
+    for(int i=0;i<n;i++){
+        string word;
+
+        while(i<n && s[i]!=' '){
+            word+=s[i];
+            i++;
+        }
+        reverse(word.begin(),word.end());
+        if(word.length()>0){
+            ans+=' '+word;
+        }
+    }
+    return ans.substr(1);
+
+}
+
+int main(){
+    string s;
+    getline(cin,s);
+    cout<<reversword(s)<<endl;
+    return 0;
+}
